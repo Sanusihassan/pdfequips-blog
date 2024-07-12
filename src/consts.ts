ba7a9495d86418579f9995182d20852fb3e583e7
Sanusi Hassan <sanusihassan122@gmail.com>
2024-07-12 04:35:21 +0300

    updated metadata

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,4 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'PDFEquips blog - Easy Online PDF Tools';
+export const SITE_DESCRIPTION = "Make your PDFs work for you with PDFEquips. Edit, convert, merge, and lock PDFs online. Easy-to-use tools that'll save you time and hassle.";
