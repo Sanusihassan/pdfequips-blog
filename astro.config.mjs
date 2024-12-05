48aa45b38416f13b31eb2220895299929e37fc17
Sanusi Hassan <sanusihassan122@gmail.com>
2024-12-05 17:34:42 +0100

    uploading to try build on windows

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,22 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 import react from "@astrojs/react";
-
+// fetch-subscription-status
 // https://astro.build/config
 export default defineConfig({
+  // build: {
+  //   rollupOptions: {
+  //     external: ['fetch-subscription-status'], // Add the package name here
+  //   },
+  // },
+  vite: {
+    optimizeDeps: {
+      include: ['fetch-subscription-status', 'react-icons'],
+    },
+    ssr: {
+      noExternal: ['fetch-subscription-status', 'react-icons']
+    }
+  },
   site: 'https://www.pdfequips.com',
   integrations: [mdx(), sitemap(), react()],
   base: import.meta.env.PROD ? "blog" : ""
