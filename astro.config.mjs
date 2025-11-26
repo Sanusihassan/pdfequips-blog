5a1682f467f0cb01bc62d8e8fa6247edaf8f02cf
Sanusi Hassan <sanusihassan122@gmail.com>
2025-11-26 05:07:22 +0400

    updated navbar & footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,25 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 import react from "@astrojs/react";
-// fetch-subscription-status
+//
 // https://astro.build/config
 export default defineConfig({
   // build: {
   //   rollupOptions: {
-  //     external: ['fetch-subscription-status'], // Add the package name here
+  //     external: [''], // Add the package name here
   //   },
   // },
   vite: {
     optimizeDeps: {
-      include: ['fetch-subscription-status', 'react-icons'],
+      include: ["react-icons"],
     },
     ssr: {
-      noExternal: ['fetch-subscription-status', 'react-icons', 'pdfequips-navbar']
-    }
+      noExternal: ["react-icons", "pdfequips-navbar"],
+    },
   },
-  site: 'https://www.pdfequips.com',
+  site: "https://www.pdfequips.com",
   integrations: [mdx(), sitemap(), react()],
-  base: import.meta.env.PROD ? "blog" : ""
+  base: import.meta.env.PROD ? "blog" : "",
 });
